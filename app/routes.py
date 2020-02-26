91b5d1f03bfe7ca1e16bd4f00a08d6a463c7a9b6
Rory Brown <rorybrown660@gmail.com>
2020-02-26 10:22:55 +0000

    added portfolio

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template
-from app import app
+from app import app, portfolio
+
+pf = portfolio.Portfolio.fromFile("app/data/portfolio.json")
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    repos = pf.getRepos()
+    return render_template("index.html", repos = repos)
