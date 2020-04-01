dd1bc6d3d561061f6ade7e892f5cabbd8dbb1302
Rory Brown <rorybrown660@gmail.com>
2020-04-01 10:22:25 +0100

    added tutoring page

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from app import app, portfolio
 
-pf = portfolio.Portfolio.fromFile("app/data/portfolio.json")
+# pf = portfolio.Portfolio.fromFile("app/data/portfolio.json")
 
 @app.route("/")
 @app.route("/index")
 def index():
-    repos = pf.getRepos()
-    return render_template("index.html", repos = repos)
+    return render_template("index.html")
+
+@app.route("/tutoring")
+def tutoring():
+    return render_template("tutoring.html")
