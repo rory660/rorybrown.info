cbcca4c6d48777aa9245d06a539f1b609671a33f
rory660 <rorybrown660@gmail.com>
2021-02-12 23:26:58 +0000

    redid entire site

--- app/src/index.js
+++
@@ -1,9 +0,0 @@
-/*jshint esversion: 9 */
-/*jshint asi: true*/
-
-import React from "react"
-import ReactDOM from "react-dom"
-
-import App from "./app.jsx"
-
-ReactDOM.render(<App></App>, document.getElementById("root"))
