2d09d4962eb4beca9e0898efcdd535c65da5919a
rory660 <rorybrown660@gmail.com>
2021-03-27 15:52:31 +0000

    css rubbish

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ function draw(){
     texture(headsetTexture)
     specularMaterial(color(HEADSET_COLOUR))
     let scaleAmount = MODEL_SCALE
-    if(window.innerWidth < 800){
+    if(window.innerWidth < 500){
+        scaleAmount /= 3
+    }
+    else if(window.innerWidth < 800){
         scaleAmount /= 2
     }else if(window.innerWidth < 1500) scaleAmount /= 1.5
 
