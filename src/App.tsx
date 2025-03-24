66507c3935dc8038416babce470d684694fb5967
Rory Brown <contact@rorybrown.info>
2025-03-24 23:20:59 +0000

    feat: hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import './App.css'
+import GradientText from "./components/GradientText";
+import ScrollVelocity from "./components/ScrollVelocity";
 
 function App() {
-
   return (
-    <>
-    <div>
-      <h1 className='font-primary text-red-500'>test yo</h1>
+    <div className="w-full flex flex-col items-center">
+      <div className="h-screen w-full flex flex-col justify-between">
+        <ScrollVelocity
+          texts={["The Sickest Dev", "Probably Ever"]}
+          velocity={10}
+          className="font-primary text-zinc-900"
+        />
+        <div className="w-full flex flex-col justify-center items-center gap-4 font-extrabold">
+          <h1 className="font-primary text-white">Rory Brown</h1>
+          <h2 className="font-primary text-white italic">
+            <GradientText
+              colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+              animationSpeed={3}
+              showBorder={false}
+            >
+              - The sickest dev probably ever -
+            </GradientText>
+          </h2>
+        </div>
+        <ScrollVelocity
+          texts={["The Sickest Dev", "Probably Ever"]}
+          velocity={10}
+          className="font-primary text-zinc-900"
+        />
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
