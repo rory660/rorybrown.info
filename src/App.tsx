f620e53039fd6d8daab9bdd94e14d350e6e933ed
Rory Brown <contact@rorybrown.info>
2025-03-24 22:57:44 +0000

    feat: initial vite commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1 className='font-primary text-red-500'>test yo</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
